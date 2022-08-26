1ae0f8f6caca3bc3bf0c12b7884e8ad932203018
barani <root@ip-172-31-45-207.ap-south-1.compute.internal>
2022-08-26 11:24:55 +0000

    hello india

--- flaskpro.py
+++ flaskpro.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return '<h1>Hello, World!</h1>'
+    return '<h2>Hello, India.!</h2>'
 if __name__ =="__main__":
-    app.run(host='0.0.0.0',port=5052)
+    app.run(host='0.0.0.0',port=5051)
 
